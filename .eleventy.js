09865d949ef2ad1448c29ca918008bed99cbf6a5
Nicolas Fédou <nicolas.fedou@gmail.com>
2022-10-21 15:57:10 +0200

    vault backup: 2022-10-21 15:57:10
    Affected files:
    .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,19 +2,17 @@ const markdownIt = require('markdown-it');
 
 module.exports = function(eleventyConfig) {
     
-/*     const markdownItOptions = {
+    const markdownItOptions = {
         html: true,
         xhtmlOut: true,
         linkify: true
     };
 
- */
-/*     const md = markdownIt(markdownItOptions)
+    const md = markdownIt(markdownItOptions)
     .use(require('markdown-it-footnote'))
     .use(require('markdown-it-obsidian'))
     .use(require('markdown-it-highlightjs'), { inline: true })
     .use(require('markdown-it-attrs'))
- */
 /*     .use(function(md) {
         // Recognize Mediawiki links ([[text]])
         md.linkify.add("[[", {
@@ -32,20 +30,12 @@ module.exports = function(eleventyConfig) {
         return md.render(string)
     })
  */
-    eleventyConfig.amendLibrary('md', md => md
-        .enable("html")
-        .enable("xhtmlOut")
-        .enable("linkify")
-        .use(require('markdown-it-footnote'))
-        .use(require('markdown-it-obsidian'))
-        .use(require('markdown-it-highlightjs'), { inline: true })
-        .use(require('markdown-it-attrs'))
-    );
+    eleventyConfig.setLibrary('md', md);
     
-    eleventyConfig.addCollection("notes", function (collection) {
-        return collection.getFilteredByGlob(["notes/**/*.md", "index.md"]);
+/*     eleventyConfig.addCollection("notes", function (collection) {
+        return collection.getFilteredByGlob(["notes/** /*.md", "index.md"]);
     });
-    
+ */    
     eleventyConfig.addPassthroughCopy('assets');
     eleventyConfig.setUseGitIgnore(false);
 
